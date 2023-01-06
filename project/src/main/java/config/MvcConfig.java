67d7058f8950052eec471dd2d07ed4cc50c2d32e
ho <gkgkghdeh21@naver.com>
2023-01-06 17:48:42 +0900

    1

--- project/src/main/java/config/MvcConfig.java
+++ project/src/main/java/config/MvcConfig.java
@@ -1,8 +1,12 @@
 package config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
